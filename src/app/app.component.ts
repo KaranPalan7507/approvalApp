02faa6e815d8d4b78b6ee719687b696457fff680
Karan Palan <karanpalan7507@gmail.com>
2018-12-26 11:48:39 +0530

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,44 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpModule, Http } from '@angular/http';
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'app';
+export class AppComponent implements OnInit {
+  public dataLoaded: boolean = false;
+  public selectedIndex = 0;
+  public headers = ["ALL DUE", "PENDING", "AUTO APPROVAL", "COMPLETED"];
+  public data: Array<any> = [];
+  constructor(private http: Http) {
+
+  }
+
+  public ngOnInit(): void {
+    this.http.get("./assets/data.json").subscribe((response) => {
+      const data = response.json();
+      console.log(data);
+      this.data.push((data.pending).concat(data.autoApprove));
+      this.data.push(data.pending);
+      this.data.push(data.autoApprove);
+      this.data.push(data.completed);
+      console.log(this.data);
+      this.dataLoaded = true;
+    })
+  }
+
+  public swipeLeft(): void {
+    if (this.selectedIndex < this.headers.length - 1) {
+      this.selectedIndex++;
+    }
+  }
+
+  public swipeRight(): void {
+    if (this.selectedIndex > 0) {
+      this.selectedIndex--;
+    }
+  }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,40 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpModule } from '@angular/http';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
+import { HammerGestureConfig, HAMMER_GESTURE_CONFIG } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { ListItemComponent } from './list-item/list-item.component';
+import { MatTabsModule } from '@angular/material';
+import { ListComponent } from './list/list.component';
+import {MatButtonModule} from '@angular/material/button';
 
+export class MyHammerConfig extends HammerGestureConfig {
+  overrides = <any>{
+  }
+}
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    ListItemComponent,
+    ListComponent
   ],
   imports: [
-    BrowserModule
+    BrowserAnimationsModule,
+    BrowserModule,
+    HttpModule,
+    MatTabsModule,
+    MatButtonModule
   ],
-  providers: [],
+  providers: [{
+    provide: HAMMER_GESTURE_CONFIG,
+    useClass: MyHammerConfig
+  }],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
